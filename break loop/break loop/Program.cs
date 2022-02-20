21c3b56489a47c8d04ef78ef7209e3c83681d952
Satyam-sagar2630 <99909799+Satyam-sagar2630@users.noreply.github.com>
2022-02-21 00:01:49 +0530

    Add files via upload

---
+++ break loop/break loop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace app
+{
+    class Program
+    {
+        static void Main(string[]arg)
+        {
+            for(int i=1; i<=10;i++)
+            {
+                if(i==5)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
